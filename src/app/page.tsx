2f30b34aa7007f72179ef0e2c37b478ce76205d6
Kevin Zoltany <kzoltany@gmail.com>
2025-03-11 15:50:43 -0700

    proast logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+import Image from 'next/image'
 import { UselessButton } from '~/components/useless-button'
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-center">
+        <main className="flex min-h-screen relative flex-col items-center justify-center">
+            <Image
+                src="/Punk.png"
+                alt="Punkroast"
+                width={100}
+                height={100}
+                className="top-5 left-5 absolute"
+            />
             <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
                 <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
                     Nothing to see here
